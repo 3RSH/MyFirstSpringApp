86d030e110b8d085ac2353b659a2249f5cda4490
3RSH/JeS <diz.domination@gmail.com>
2021-04-22 10:51:37 +0300

    auth/check response added

--- src/main/java/main/controller/ApiAuthController.java
+++ src/main/java/main/controller/ApiAuthController.java
@@ -1,5 +1,7 @@
 package main.controller;
 
+import main.api.response.CheckResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class ApiAuthController {
 
+  private final CheckResponse userStatus;
+
+  public ApiAuthController(CheckResponse userStatus) {
+    this.userStatus = userStatus;
+  }
+
+  @GetMapping("/check")
+  private CheckResponse check() {
+    return userStatus;
+  }
 }
