6ae78bd2518dfe93a4cc8f13115ec7538c4f1026
3RSH/JeS <diz.domination@gmail.com>
2021-05-18 23:48:31 +0300

    api/auth/captcha response added

--- src/main/java/main/controller/ApiAuthController.java
+++ src/main/java/main/controller/ApiAuthController.java
@@ -1,6 +1,9 @@
 package main.controller;
 
+import java.io.IOException;
+import main.api.response.CaptchaResponse;
 import main.api.response.CheckResponse;
+import main.service.captcha.CaptchaServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiAuthController {
 
   private final CheckResponse userStatus;
+  private final CaptchaServiceImpl captchaService;
 
 
-  public ApiAuthController(CheckResponse userStatus) {
+  public ApiAuthController(CheckResponse userStatus,
+      CaptchaServiceImpl captchaService) {
     this.userStatus = userStatus;
+    this.captchaService = captchaService;
   }
 
 
   @GetMapping("/check")
   private CheckResponse check() {
     return userStatus;
+  }
+
+  @GetMapping("/captcha")
+  private CaptchaResponse getCaptcha() throws IOException {
+    return captchaService.getNewCaptcha();
   }
 }
---
+++ src/main/java/main/service/captcha/CaptchaServiceImpl.java
@@ -0,0 +1,80 @@
+package main.service.captcha;
+
+import com.github.cage.Cage;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Base64;
+import javax.imageio.ImageIO;
+import main.api.response.CaptchaResponse;
+import main.model.CaptchaCode;
+import main.repository.captcha.CaptchaRepository;
+import org.imgscalr.Scalr;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CaptchaServiceImpl implements CaptchaService {
+
+  private final CaptchaRepository captchaRepository;
+
+  @Value("${captcha.lifetimeBySec}")
+  private long captchaLifetime;
+
+
+  public CaptchaServiceImpl(
+      @Qualifier("CaptchaRepository") CaptchaRepository captchaRepository) {
+    this.captchaRepository = captchaRepository;
+  }
+
+
+  @Override
+  public CaptchaResponse getNewCaptcha() {
+    Timestamp timeThreshold = Timestamp.valueOf(LocalDateTime.now()
+        .minusSeconds(captchaLifetime));
+
+    captchaRepository.deleteOldCaptcha(timeThreshold);
+
+    Cage cage = new Cage();
+    String code = cage.getTokenGenerator().next();
+    BufferedImage captchaImage = cage.drawImage(code);
+
+    byte[] bytesOfImage = compressImageAndGetByteArray(captchaImage);
+    String encodeImage = Base64.getEncoder().encodeToString(bytesOfImage);
+
+    String secret = String.valueOf(code.hashCode());
+
+    CaptchaCode captchaCode = new CaptchaCode();
+
+    captchaCode.setCode(code);
+    captchaCode.setSecretCode(secret);
+    captchaCode.setTime(new Timestamp(System.currentTimeMillis()));
+
+    captchaRepository.saveAndFlush(captchaCode);
+
+    CaptchaResponse captchaResponse = new CaptchaResponse();
+
+    captchaResponse.setImage("data:captchaImage/png;base64, " + encodeImage);
+    captchaResponse.setSecret(secret);
+
+    return captchaResponse;
+  }
+
+  private byte[] compressImageAndGetByteArray(BufferedImage image) {
+    byte[] result = new byte[0];
+    image = Scalr.resize(image, Scalr.Method.AUTOMATIC, Scalr.Mode.AUTOMATIC,
+        100, 35, Scalr.OP_ANTIALIAS);
+
+    try (ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+      ImageIO.write(image, "png", out);
+      result = out.toByteArray();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return result;
+  }
+}
